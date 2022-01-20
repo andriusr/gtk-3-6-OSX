a83ae07d0b440fc5f672a7230a7564c78a1a6bd4
Andrius Rinkevičius <andrius.r@qlinstruments.com>
2022-01-20 11:10:32 +0200

    Revert "Maybe it will work"
    This reverts commit c06e6616b627a975398aa61dba435e113af5df54.

--- gtk/gtktextview.c
+++ gtk/gtktextview.c
@@ -27,6 +27,9 @@
 
 #include <string.h>
 
+#include "gdk/gdk.h"
+#include "gtkquartz.h"
+#include "gdk/quartz/gdkquartz.h"
 #include "gdk/quartz/gdkwindow-quartz.h"
 
 #define WINDOW_IS_TOPLEVEL(window)		     \
